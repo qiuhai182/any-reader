d3370d7e8610a3187c0433980e1b2fd4e4f64eee
邱海 <2810617217@qq.com>
2026-06-23 11:48:08 +0800

    去重快捷上传

--- vsix-build.bat
+++ vsix-build.bat
@@ -2,27 +2,28 @@
 chcp 65001 >nul 2>&1
 
 echo ========== 步骤1：安装依赖 ==========
-@REM call pnpm-npm-vsce-install.bat
+call pnpm-npm-vsce-install.bat
 
 echo.
 echo ========== 步骤2：构建核心模块 ==========
-pnpm.cmd build
+call pnpm.cmd build
 
 echo.
 echo ========== 步骤3：构建 VSCode Web 模板 ==========
-pnpm.cmd vscode:build-tpl
+call pnpm.cmd vscode:build-tpl
 
 echo.
 echo ========== 步骤4：构建 VSCode 插件源码 ==========
 cd packages\vscode
-copy package.json package.json.build-bak >nul
-pnpm.cmd build
+call pnpm.cmd build
 cd ..\..
 
 echo.
 echo ========== 步骤5：生成 VSIX 包 ==========
 cd packages\vscode
-pnpm.cmd run pack
+copy package.json package.json.build-bak >nul
+call pnpm.cmd run vscode:prepublish
+call pnpm.cmd exec vsce package --no-dependencies
 copy /y package.json.build-bak package.json >nul
 del package.json.build-bak
 cd ..\..
